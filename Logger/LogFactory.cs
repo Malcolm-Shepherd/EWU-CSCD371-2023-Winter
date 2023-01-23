d00137de2233562607fb0f50a40e1c78a038bb58
Clockknight <hello@tylerhanyinwang.com>
2023-01-22 23:14:49 -0800

    Add ConfigureFileLogger to CreateLogger
    Removed static attribute from createlogger too.

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,15 +2,24 @@
 {
     public class LogFactory : BaseLogger
     {
-        public static BaseLogger CreateLogger(string className)
+
+        private string path;
+
+        public BaseLogger CreateLogger(string className, string inputPath)
         {
             Name = className;
+            ConfigureFileLogger(inputPath);
             return null;
         }
 
         public override void Log(LogLevel logLevel, string message)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void ConfigureFileLogger(string inputPath)
+        {
+            path = inputPath;
         }
     }
 }
