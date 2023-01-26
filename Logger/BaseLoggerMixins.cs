0b7204d617af1a618b66d86296780a9d6ff5dcd7
Clockknight <hello@tylerhanyinwang.com>
2023-01-25 21:56:27 -0800

    Include nameof() inside of baseloggermixins

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -7,29 +7,45 @@ namespace Logger
     {
         public static void Error(this BaseLogger logger, string input, int parameter)
         {
-            if (logger is null || input is null || parameter is 0)
+            if (input is null || parameter is 0)
                 throw new ArgumentNullException();
+            if (logger == null) 
+            { 
+                throw new ArgumentNullException(nameof(logger)); 
+            }
             input = String.Format(input, parameter);   
             logger.Log(LogLevel.Error, input);
         }
         public static void Warning(this BaseLogger logger, string input, int parameter)
         {
-            if (logger is null || input is null || parameter is 0)
+            if (input is null || parameter is 0)
                 throw new ArgumentNullException();
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
             input = String.Format(input, parameter);
             logger.Log(LogLevel.Error, input);
         }
         public static void Information(this BaseLogger logger, string input, int parameter)
         {
-            if (logger is null || input is null || parameter is 0)
+            if (input is null || parameter is 0)
                 throw new ArgumentNullException();
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
             input = String.Format(input, parameter);
             logger.Log(LogLevel.Error, input);
         }
         public static void Debug(this BaseLogger logger, string input, int parameter)
         {
-            if (logger is null || input is null || parameter is 0)
+            if (input is null || parameter is 0)
                 throw new ArgumentNullException();
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
             input = String.Format(input, parameter);
             logger.Log(LogLevel.Error, input);
         }
