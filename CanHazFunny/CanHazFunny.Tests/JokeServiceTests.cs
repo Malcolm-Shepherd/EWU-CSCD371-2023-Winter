88aec7fcad5b464266956b7a4b344f2d63ae29bf
Malcolm-Shepherd <mshepherd1@ewu.edu>
2023-01-30 13:45:00 -0800

    IJokeService Implementation Tested

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
 
 namespace CanHazFunny.Tests
 {
@@ -6,6 +8,10 @@ namespace CanHazFunny.Tests
     public class JokeServiceTests
     {
         //test if jokeservice class implements IJokeservice
-
+        [TestMethod]
+        public void JokeService_Implements_IJokeService_True()
+        {
+            Assert.IsTrue(typeof(JokeService).GetInterfaces().Contains(typeof(IJokeService)));
+        }
     }
 }
