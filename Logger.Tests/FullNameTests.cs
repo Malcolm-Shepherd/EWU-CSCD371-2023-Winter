95bad075348d88e939bf79a191494320c465b095
Malcolm-Shepherd <mshepherd1@ewu.edu>
2023-02-05 11:56:22 -0800

    FullName record implemented and tested

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class FullNameTests
+{
+    [TestMethod]
+    public void FullName_Initializes_Pass()
+    {
+        string first = "Malcolm";
+        string middle = "O";
+        string last = "Shepherd";
+        FullName me = new(first, last, middle);
+        
+        Assert.AreEqual<string>(first, me.FirstName);
+        Assert.AreEqual<string>(last, me.LastName);
+        Assert.AreEqual<string>(middle, me.MiddleName!);
+    }
+
+    [TestMethod]
+    public void FullName_NullCheck_Pass()
+    {
+        ArgumentNullException firstNull = null;
+
+        try { FullName first = new(null, "a", "b"); }
+        catch (ArgumentNullException ex) { firstNull = ex; }
+        Assert.IsNotNull(firstNull);
+
+
+        ArgumentNullException lastNull = null;
+
+        try { FullName last = new("c", null, "d"); }
+        catch (ArgumentNullException ex) { lastNull = ex; }
+        Assert.IsNotNull(lastNull);
+
+
+        ArgumentNullException middleNull = null;
+
+        try { FullName middle = new("e", "f", null); }
+        catch (ArgumentNullException ex) { middleNull = ex; }
+        Assert.IsNull(middleNull);
+    }
+}
+
--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,6 +1,11 @@
 ﻿namespace Logger;
-public record struct FullName(string FirstName, string LastName, string? MiddleName = null)
+public record class FullName(string FirstName, string LastName, string? MiddleName = null)
 {
+/*Reference type as different people can have the same full name.
+  Read only as it is very rare for people to need to change their name.
+  Null checks on First and Last as they are needed.
+  Middle name is optional, so no checks are needed.
+ */
     public string FirstName { get; } = FirstName??throw new ArgumentNullException(nameof(FirstName));
     public string LastName { get; } = LastName??throw new ArgumentNullException(nameof(LastName));
     public string? MiddleName { get; } = MiddleName;
