b82911e6fe2b728574f5387a8948c3bb90a03a08
Clockknight <hello@tylerhanyinwang.com>
2023-02-06 20:07:02 -0800

    Update StorageTests.cs

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -6,6 +6,28 @@ namespace Logger.Tests;
 [TestClass]
 public class StorageTests
 {
+    #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    Storage storage;
+    
+    Book book = new Book("Princess Bride");
+    Employee employee = new Employee(new FullName("Inigo", "Montonoya"));
+    Student student = new Student(new FullName("Prince", "Humperdinck"));
+
+    [TestInitialize]
+    public void testInit()
+    {
+        storage = new Storage();
+        storage.Add(book);
+        storage.Add(employee);
+        storage.Add(student);
+    }
+
+    [TestMethod]
+    public void Storage_Get_Returns_Expected_ID()
+    {
+        
+    }
+
     [TestMethod]
     public void Storage_Add_Adds_Entity()
     {
@@ -17,10 +39,6 @@ public void Storage_Remove_Removes_Entity()
     [TestMethod]
 public void Storage_Contains_Returns_Expected_Item()
     {
-}
-[TestMethod]
-public void Storage_Get_Returns_Expected_ID ()
-    {
-}
+    }
 
 }
