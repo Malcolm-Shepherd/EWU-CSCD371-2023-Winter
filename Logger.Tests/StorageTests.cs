af2aa6ff7cf87d095da0d14ff6cda26fc55c3e8a
Clockknight <hello@tylerhanyinwang.com>
2023-02-06 19:49:13 -0800

    Create StorageTests.cs

---
+++ Logger.Tests/StorageTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class StorageTests
+{
+    [TestMethod]
+    public void Storage_Add_Adds_Entity()
+    {
+    }
+[TestMethod]
+public void Storage_Remove_Removes_Entity()
+    {
+    }
+    [TestMethod]
+public void Storage_Contains_Returns_Expected_Item()
+    {
+}
+[TestMethod]
+public void Storage_Get_Returns_Expected_ID ()
+    {
+}
+
+}
