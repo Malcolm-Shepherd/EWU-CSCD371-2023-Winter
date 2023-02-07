9b09f9238c5e3bc8145d87cf3e90c937d91edf61
Malcolm-Shepherd <mshepherd1@ewu.edu>
2023-02-06 20:36:26 -0800

    Storage class unit tested

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -6,12 +6,14 @@ namespace Logger.Tests;
 [TestClass]
 public class StorageTests
 {
+    // We know 'storage' will be intialized when we use it, so the warning does not apply.
     #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     Storage storage;
     
     Book book = new Book("Princess Bride");
     Employee employee = new Employee(new FullName("Inigo", "Montonoya"));
     Student student = new Student(new FullName("Prince", "Humperdinck"));
+    #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     [TestInitialize]
     public void testInit()
@@ -25,7 +27,14 @@ public class StorageTests
     [TestMethod]
     public void Storage_Get_Returns_Expected_ID()
     {
+        Guid guid = Guid.NewGuid();
+        Employee testMan = new(new("Test", "Man")) { Id = guid };
+        storage.Add(testMan);
 
+        IEntity storedEmp = storage.Get(guid)!;
+
+        Assert.IsNotNull(storedEmp);
+        Assert.AreEqual(testMan, storedEmp);
     }
 
     [TestMethod]
